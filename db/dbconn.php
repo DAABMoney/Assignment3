21bed440a7b100fd6e6433dcee5def5bfdcd5fbb
Duane <duane.brk@gmail.com>
2022-12-21 21:40:37 -0500

    Test server

--- db/dbconn.php
+++ db/dbconn.php
@@ -1,23 +1,23 @@
 <?php
 
         /**Production Server*/
-        
+        /*
     $host='applied-web.mysql.database.azure.com';
     $db='subscribe_duane';
     $user='appliedweb_user@applied-web';
     $password='P@ssword1';
     $charset='utf8mb4';
     $dsn="mysql:host=$host;dbname=$db;charset=$charset";
-        
+     */   
     /**  Testing Server*/
-    /*
+    
     $host='127.0.0.1';
     $db='subscribe_db';
     $user='root';
     $password='';
     $charset='utf8mb4';
     $dsn="mysql:host=$host;dbname=$db;charset=$charset";
-    */
+    
     try{
         $pdo = new PDO($dsn, $user, $password);
         $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
